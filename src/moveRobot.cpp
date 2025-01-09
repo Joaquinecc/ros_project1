8c4780a45d67004d819d74d08fdde5cac8a42acc
joaquinecc <joaquinecc@gmail.com>
2025-01-09 16:34:49 +0100

    robot follower implementation

--- src/moveRobot.cpp
+++ src/moveRobot.cpp
@@ -51,12 +51,15 @@ int T_WAIT = 150;               // Time to wait between algorithm decisions (ms)
 /**
  * ROS Publisher and Global Variables
  */
-ros::Publisher cmd_vel_pub;      // Publisher for movement commands
+ros::Publisher cmd_leader_vel_pub;      // Publisher for movement commands
+ros::Publisher cmd_follower_vel_pub;      // Publisher for movement commands
 ros::Time start;
 
 geometry_msgs::Point goal_coord; // Global goal position
-geometry_msgs::Point leader_coords; // Global goal position
+geometry_msgs::Point leader_coord; // Global goal position
 double leader_orientationr;     // Current orientation in radians
+geometry_msgs::Point follower_coord; // Global goal position
+double follower_orientationr;     // Current orientation in radians
 
 // State Flags
 bool DEBUG = true;               // Debug mode flag
@@ -238,18 +241,18 @@ void callbackLaserLeader(const sensor_msgs::LaserScan& most_intense) {
     if (isTheregoal) { // If there is a goal, we move toward it
 
         if (ALGOR == 1) {
-                cmd_vel = algo1(most_intense,leader_coords,leader_orientationr,goal_coord);
-                cmd_vel_pub.publish(cmd_vel);
+                cmd_vel = algo1(most_intense,leader_coord,leader_orientationr,goal_coord);
+                cmd_leader_vel_pub.publish(cmd_vel);
         }else if (ALGOR == 2) { // Potential fields algorithm
              ros::Time current_time = ros::Time::now();
 			// Check if enough time has passed for algo2 decisions
             if ((current_time - last_decision_time).toSec() * 1000 >= T_WAIT) {
                 // Execute algo2 for potential fields algorithm
-                cmd_vel = algo2(most_intense,leader_coords,leader_orientationr,goal_coord);
+                cmd_vel = algo2(most_intense,leader_coord,leader_orientationr,goal_coord);
                 // Update the last decision time
                 last_decision_time = current_time;
                 //Update velocities
-                cmd_vel_pub.publish(cmd_vel);
+                cmd_leader_vel_pub.publish(cmd_vel);
             }
 		}
         ros::spinOnce();
@@ -257,22 +260,73 @@ void callbackLaserLeader(const sensor_msgs::LaserScan& most_intense) {
     }
 }
 
+void callbackLaserFollower(const sensor_msgs::LaserScan& most_intense) {
+    if (ROBOT_ROL == 0) return;
+    static ros::Time last_decision_time = ros::Time::now(); // Track last decision time
+    geometry_msgs::Twist cmd_vel; // Initialize cmd_vel
+    double distance_to_leader = sqrt(pow(leader_coord.x - follower_coord.x, 2) + pow(leader_coord.y - follower_coord.y, 2));
+    if(DEBUG){
+        ROS_INFO("DISTANCE TO LEADER: %.2f", distance_to_leader);
+    }
+    if(distance_to_leader>DIST_LEADER){//Check distance with the leader
+        if (ALGOR == 1) {
+                cmd_vel = algo1(most_intense,follower_coord,follower_orientationr,leader_coord);
+                cmd_follower_vel_pub.publish(cmd_vel);
+                ros::spinOnce();
+        }else if (ALGOR == 2) { // Potential fields algorithm
+            ros::Time current_time = ros::Time::now();
+            // Check if enough time has passed for algo2 decisions
+            if ((current_time - last_decision_time).toSec() * 1000 >= T_WAIT) {
+                // Execute algo2 for potential fields algorithm
+                cmd_vel = algo2(most_intense,follower_coord,follower_orientationr,leader_coord);
+                // Update the last decision time
+                last_decision_time = current_time;
+                //Update velocities
+                cmd_follower_vel_pub.publish(cmd_vel);
+                ros::spinOnce();
+            }
+        }
+    }else{ //To close to leader, stop
+        ros::Time current_time = ros::Time::now();
+        cmd_vel.linear.x = 0.0;
+        cmd_vel.linear.y = 0.0;
+        cmd_vel.linear.z = 0.0;
+        cmd_vel.angular.x = 0.0;
+        cmd_vel.angular.y = 0.0;
+        cmd_vel.angular.z = 0.0;
+        // Update the last decision time
+        last_decision_time = current_time;
+        //Update velocities
+        cmd_follower_vel_pub.publish(cmd_vel);
+        ros::spinOnce();
+
+    }
+
+}
 
 
 void callbackOdomLeader(const nav_msgs::Odometry odom) {
-	leader_coords.x = odom.pose.pose.position.x;
-	leader_coords.y = odom.pose.pose.position.y;
+	leader_coord.x = odom.pose.pose.position.x;
+	leader_coord.y = odom.pose.pose.position.y;
 	double current_z=odom.pose.pose.orientation.z;
 	double current_w=odom.pose.pose.orientation.w;
 	leader_orientationr=(2.0*atan2(current_z,current_w));
 
 	if (isTheregoal) {
-		double distance_to_goal = sqrt(pow(goal_coord.x - leader_coords.x, 2) + pow(goal_coord.y - leader_coords.y, 2));
+		double distance_to_goal = sqrt(pow(goal_coord.x - leader_coord.x, 2) + pow(goal_coord.y - leader_coord.y, 2));
 		if (distance_to_goal < D_OBJ) {
 			isTheregoal = false;
 			ROS_INFO("Goal point achieved.");
 		}
 	}
+}
+void callbackOdomFollower(const nav_msgs::Odometry odom) {
+    if (ROBOT_ROL == 0) return;
+	follower_coord.x = odom.pose.pose.position.x;
+	follower_coord.y = odom.pose.pose.position.y;
+	double current_z=odom.pose.pose.orientation.z;
+	double current_w=odom.pose.pose.orientation.w;
+	follower_orientationr=(2.0*atan2(current_z,current_w));
 }
 
 /**
@@ -340,20 +394,25 @@ int main(int argc, char **argv) {
 
 
 
-    // Dynamically construct topic names using ID_ROBOT
+    // Dynamically construct topic names using Leader id
     std::stringstream ss;
     ss << "robot_" << (ROBOT_ROL == 0 ? ID_ROBOT : ID_LEADER);
-    std::string robot_leader = ss.str(); // Example: "robot_1" for ID_ROBOT=1
+    std::string robot_leader = ss.str();
     ROS_INFO("robot_leader: %s", robot_leader.c_str());
-
-    // Get the goal objective of the robot
     ros::Subscriber goal_sub = nh.subscribe("myGoals", 10, callbackmyGoal);
-    // Publisher for robot velocity
-    cmd_vel_pub = nh.advertise<geometry_msgs::Twist>(robot_leader + "/cmd_vel", 10);
-    // Subscriber for robot's odometry
+    cmd_leader_vel_pub = nh.advertise<geometry_msgs::Twist>(robot_leader + "/cmd_vel", 10);
     ros::Subscriber odom_sub = nh.subscribe(robot_leader + "/odom", 10, callbackOdomLeader);
-    // Subscriber for robot's laser scan
     ros::Subscriber laser_sub = nh.subscribe(robot_leader + "/base_scan_1", 1, callbackLaserLeader);
+
+    //Follower
+    std::stringstream ss_follower;
+    ss_follower << "robot_" << ID_ROBOT;
+    std::string robot_follower = ss_follower.str();
+    ROS_INFO("robot_follower: %s", robot_follower.c_str());
+    cmd_follower_vel_pub = nh.advertise<geometry_msgs::Twist>(robot_follower + "/cmd_vel", 10);
+    ros::Subscriber odom_sub_follower = nh.subscribe(robot_follower + "/odom", 10, callbackOdomFollower);
+    ros::Subscriber laser_sub_follower = nh.subscribe(robot_follower + "/base_scan_1", 1, callbackLaserFollower);
+
 
 	start = ros::Time::now();
 	ros::spin();
