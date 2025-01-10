e37d7f8b3885973506063a7f14ca966ee59474df
joaquinecc <joaquinecc@gmail.com>
2025-01-10 16:04:19 +0100

    start when is goal follower

--- src/moveRobot.cpp
+++ src/moveRobot.cpp
@@ -261,7 +261,7 @@ void callbackLaserLeader(const sensor_msgs::LaserScan& most_intense) {
 }
 
 void callbackLaserFollower(const sensor_msgs::LaserScan& most_intense) {
-    if (ROBOT_ROL == 0) return;
+    if (ROBOT_ROL == 0 && !isTheregoal) return;
     static ros::Time last_decision_time = ros::Time::now(); // Track last decision time
     geometry_msgs::Twist cmd_vel; // Initialize cmd_vel
     double distance_to_leader = sqrt(pow(leader_coord.x - follower_coord.x, 2) + pow(leader_coord.y - follower_coord.y, 2));
@@ -321,7 +321,7 @@ void callbackOdomLeader(const nav_msgs::Odometry odom) {
 	}
 }
 void callbackOdomFollower(const nav_msgs::Odometry odom) {
-    if (ROBOT_ROL == 0) return;
+    if (ROBOT_ROL == 0 && !isTheregoal) return;
 	follower_coord.x = odom.pose.pose.position.x;
 	follower_coord.y = odom.pose.pose.position.y;
 	double current_z=odom.pose.pose.orientation.z;
